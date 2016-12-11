e45b634122d04bf7f7f01f0d3f12388a1f4a1acb
Patrick Abel <Patrick@Patricks-MacBook-Pro.local>
2016-12-11 15:56:58 -0600

    roll back on appID and secret changes.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//Add your requirements
+// Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MY_APP_ID || "ae995099-8287-41f7-90f2-2719b3be9d69";
-var appSecret = process.env.MY_APP_SECRET || "0CcK3ehvmxXTqs0omgPrKF0";
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
