cd724676d91f82dd2243387c64c0dfe1623b5b4a
Patrick Abel <Patrick@Patricks-MacBook-Pro.local>
2016-12-11 14:33:35 -0600

    clean-up of package.json file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BotWorks
+# s7-bot
 Build a bot in Node.js on Microsoft Bot Framework.
 
 Chat with this bot at [botworks.azurewebsites.net](https://botworks.azurewebsites.net) 
--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-//Add your requirements
+//Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MY_APP_ID || "Missing your app ID";
-var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+var appId = process.env.MY_APP_ID || "4f0ed808-4f5a-4476-8e4f-d49142ea7461";
+var appSecret = process.env.MY_APP_SECRET || "1yVsJh4LivYs9insqOayH9o";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
-({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
-bot.add('/', new builder.SimpleDialog( function (session) {
-session.send('Hello World');
+    ({ appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET });
+bot.add('/', new builder.SimpleDialog(function (session) {
+    session.send('Hello World');
 }));
 
 // Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.post('/API/Messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3000, function () {
-console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 server.get('/', restify.serveStatic({
